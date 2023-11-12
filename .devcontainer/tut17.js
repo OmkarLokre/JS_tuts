28268b91b9686fa81201f22215e915d3cc3cdebe
OmkarLokre <139554122+OmkarLokre@users.noreply.github.com>
2023-11-12 10:10:58 +0000

    functions-P2

---
+++ .devcontainer/tut17.js
@@ -0,0 +1,7 @@
+// multiple parameters: -> usr rest pr spread method in parameters :
+//  ... -> Rest/Spread
+
+function returnVal(...value){
+    return value;
+}
+console.log(returnVal(4,7,8,21));
