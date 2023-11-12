a63ba38fd43b9f3cbcea08d884b367d89635ef5f
OmkarLokre <139554122+OmkarLokre@users.noreply.github.com>
2023-11-12 02:44:23 +0000

    datatypes

--- .devcontainer/tut1.js
+++ .devcontainer/tut1.js
@@ -1,1 +1,19 @@
 console.log("Omkar")
+
+const accountId = 12445;
+let email = "omkar@gmail.com";
+var accountPassword = "Omkar@2003";
+city = "Thane";
+let name;
+
+
+/*
+Prefer not to use 'var' because of issue in block scope and functional scope.
+*/
+
+// accountId = 124475;  not allowed to reassign as it is declased to be const parameter
+console.log(accountId);
+console.log(accountPassword);
+console.log(email);
+console.log(city);
+console.log(name); // it will be undefined
---
+++ .devcontainer/tut2.js
@@ -0,0 +1,28 @@
+"use strict"; // 
+// alert(3+3); // cannot use this as we are not in browser we are using node JS
+
+// Code readibility should be high
+
+//  Data Types:
+
+// number  upto 2 to power of 53
+// bigint
+// String -> ""
+// null: it is not undefinrd ,it is just empty
+// undefined 
+// symbol => when uniquness is needed
+
+const accountId = 12445;
+let email = "omkar@gmail.com";
+var accountPassword = true;
+let city = "Thane";
+let name;
+
+// typeof keyword
+
+console.log(typeof(accountId));
+console.log(typeof(accountPassword));
+console.log(typeof(email));
+console.log(typeof(city));
+
+// type of null is 'object
