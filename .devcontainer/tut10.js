babb39716c5ce4ffcc973380c93dade71b292289
OmkarLokre <139554122+OmkarLokre@users.noreply.github.com>
2023-11-12 06:13:30 +0000

    date

---
+++ .devcontainer/tut10.js
@@ -0,0 +1,19 @@
+//  Dates
+
+const date = new Date();
+console.log('\n');
+console.log(date);
+console.log('\n');
+console.log(date.toString());
+console.log('\n');
+console.log(date.toDateString());
+console.log('\n');
+console.log(date.getDate());
+console.log('\n');
+console.log(date.getDay());
+console.log('\n');
+console.log(date.getFullYear());
+console.log('\n');
+let newDate = new Date(2023,2,15);
+console.log('\n');
+console.log(newDate.toDateString());
