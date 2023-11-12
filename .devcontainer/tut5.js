a78b8c5fdca0965cd8ef1679f75df3fb01962e9c
OmkarLokre <139554122+OmkarLokre@users.noreply.github.com>
2023-11-12 03:16:40 +0000

    comparisions

---
+++ .devcontainer/tut5.js
@@ -0,0 +1,13 @@
+let num = 23;
+
+// Comparisons of same data type is easy , lets try out comparision of different data types:
+
+console.log("23"==23);
+console.log("23">23);
+console.log("23"<23);
+
+console.log("--------------------------------------");
+
+console.log(null==0);
+console.log(null>0);
+console.log(null<1);
