ab654457ded7c9c81674ac815c20c0910177444f
OmkarLokre <139554122+OmkarLokre@users.noreply.github.com>
2023-11-12 03:42:00 +0000

    dataype_summary

---
+++ .devcontainer/tut6.js
@@ -0,0 +1,24 @@
+// Primitives
+
+/*
+String
+Number
+Boolean
+null
+undefined
+Symbol
+*/
+
+// Reference (NOn Primitive): 
+
+/*
+Arrays
+Object
+Functions
+*/
+
+// data type of null is 'object'
+// data type of undefined is undeifined
+// data type of function is function
+
+
