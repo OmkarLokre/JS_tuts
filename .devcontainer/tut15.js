ee7be39aa9c38119538f3432de68ef41abd2754e
OmkarLokre <139554122+OmkarLokre@users.noreply.github.com>
2023-11-12 09:42:40 +0000

    obj-P3

---
+++ .devcontainer/tut15.js
@@ -0,0 +1,14 @@
+// object destructuring  
+
+const obj ={
+    naam : "Omkar",
+    age:14,
+    location:"Thane"
+}
+
+// now if we want to access the properties of the object we wil.l call it one by one
+// alternative method below:
+
+const{naam:name}=obj;
+
+console.log(name);
