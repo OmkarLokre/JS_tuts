ef3687044899296e8f1678d82a5244012a9b33af
OmkarLokre <139554122+OmkarLokre@users.noreply.github.com>
2023-11-12 02:55:29 +0000

    data_types_conversions

---
+++ .devcontainer/tut3.js
@@ -0,0 +1,18 @@
+ let score = 33;
+console.log(typeof(33));
+
+// String converted to a number assigns 'NaN' value
+let a = "Omkar";
+console.log(Number(a));
+
+// null converted to a number assigns 0 value
+let ab = null;
+console.log(Number(ab));
+
+// true converted to a number assigns 1 value
+let abc =true;
+console.log(Number(abc));
+
+// 1 converted to true
+let b = 1;
+console.log(Boolean(b));
