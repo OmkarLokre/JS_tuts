3888bf3429649f9f71f6d7cb7ca0815cf821f03a
OmkarLokre <139554122+OmkarLokre@users.noreply.github.com>
2023-11-12 10:01:55 +0000

    functions-P1

---
+++ .devcontainer/tut16.js
@@ -0,0 +1,19 @@
+// Functions
+
+// Syntax of declaration and definition
+
+ function sayMyName(num1,num2){
+        return num1+num2;
+ }
+//  here num1,num2 are parameteres
+
+const result=sayMyName(4,8);
+ console.log(result)
+ console.log(typeof(result))
+
+ function loginUserMsg(username){
+    return `${username} just logged in`
+ }
+
+ console.log(loginUserMsg("Omkar"));
+ 
